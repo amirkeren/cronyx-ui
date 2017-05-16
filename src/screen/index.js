8f9002b0b33175f6e4bf3c2dc4502a73d624d371
Gal Bari <gal.b@taboola.com>
2017-05-16 12:10:30 +0300

    triggers list header

--- src/screen/index.js
+++ src/screen/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import searchIcon from '../../assets/svg/assets_History_2017-05-16/ic-search.svg';
+
 import './style.css';
 
 class Screen extends Component{
@@ -8,6 +10,7 @@ class Screen extends Component{
                 <div className="row triggers-header align-items-center">
                     <h1 className="col-sm-3 triggers-list-header">Triggers List</h1>
                     <div className="offset-sm-4 col-sm-4">
+                        <img src={searchIcon} alt="" className="search-icon"/>
                         <input className="search-box" placeholder="type trigger name" />
                     </div>
                 </div>
