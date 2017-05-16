2fdc0f87d2daf4f23b6006c6e14885fae190a92e
Tom Sisso <tom.s@taboola.com>
2017-05-16 12:17:46 +0300

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from "./Header/Header";
+import Sidebar from "./Sidebar/Sidebar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="App">
+            <Header/>
+            <div className="row">
+                <Sidebar className="col-3"/>
+                <div className="col-9">
+                    <p className="App-intro">
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                    </p>
+                </div>
+            </div>
+        </div>
     );
   }
 }
