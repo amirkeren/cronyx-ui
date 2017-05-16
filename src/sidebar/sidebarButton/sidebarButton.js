bb229a48fc9517e4a409cae77684e656e10ae4ac
Tom Sisso <tom.s@taboola.com>
2017-05-16 15:24:20 +0300

    sa

--- src/sidebar/sidebarButton/sidebarButton.js
+++ src/sidebar/sidebarButton/sidebarButton.js
@@ -5,7 +5,7 @@ import './sidebarBurron.css';
 class SidebarButton extends Component {
     render() {
         return (
-            <div onClick={this.props.onClick}>
+            <div onClick={this.props.onClick} className={this.props.isButtonActive ? "Button ActiveButton" : "Button"}>
                 <img className="SidebarIcon" src={this.props.img}/>
                 <h1 className={this.props.isButtonActive ? "SidebarActiveLabel" : "SidebarInactiveLabel"}>{this.props.label}</h1>
             </div>
