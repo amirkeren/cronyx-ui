543079596435687440633bacf9612e3b7480412b
Yotam Bar-on <tudmotu@gmail.com>
2017-05-16 16:27:44 +0300

    Add trigger info modal

---
+++ src/Modal/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './style.css';
+
+export default class Modal extends React.Component {
+    render () {
+        const props = this.props;
+        return <div className="cronyx-modal">
+            <div className="cronyx-modal-background"></div>
+            <div className="cronyx-modal-window">
+                <div className="cronyx-modal-title">{props.title}</div>
+                <div className="cronyx-modal-subtitle">{props.subtitle}</div>
+                <div className="cronyx-modal-content">
+                    {props.children}
+                </div>
+                <div className="cronyx-modal-footer">
+                    {props.buttons.map((btn, i) => {
+                        return <div key={i} className="cronyx-modal-button"
+                                onClick={btn.onClick}
+                                data-primary={btn.primary}>
+                            {btn.text}
+                        </div>;
+                    })}
+                </div>
+            </div>
+        </div>
+    }
+}
--- src/screen/index.js
+++ src/screen/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import searchIcon from '../../assets/svg/assets_History_2017-05-16/ic-search.svg';
 import moment from 'moment';
+import Modal from '../Modal';
+import {triggerInfo} from '../ajaxutils';
 
 import './style.css';
 
@@ -32,6 +34,16 @@ class Screen extends Component{
       });
   }
 
+    openInfoModal (trigger) {
+        const {name, group} = trigger.triggerKey;
+        triggerInfo(name, group).then(resp => {
+            this.setState({
+                currentTrigger: trigger,
+                currentTriggerInfo: resp.data
+            });
+        });
+    }
+
     render() {
         return(
             <div className="main-screen-wrapper">
@@ -62,7 +74,7 @@ class Screen extends Component{
                     </thead>
                     <tbody>
                         {this.state.triggers.map(trigger =>
-                            <tr>
+                            <tr onClick={() => this.openInfoModal(trigger)}>
                                   <td>
                                     {trigger.triggerKey.name}
                                   </td>
@@ -84,6 +96,19 @@ class Screen extends Component{
                     </tbody>
                 </table>
                 </div>
+                {this.state.currentTrigger ?
+                    <Modal title="Trigger Info"
+                            subtitle={`${this.state.currentTrigger.triggerKey.group}.${this.state.currentTrigger.triggerKey.name}`}
+                            buttons={[{
+                                text: "Close",
+                                primary: true,
+                                onClick: () => this.setState({ currentTrigger: null })
+                            }]}>
+                        <pre className="trigger-info-pre">
+                            {JSON.stringify(this.state.currentTriggerInfo, null, 4)}
+                        </pre>
+                    </Modal>
+                : null}
             </div>
         );
     }
